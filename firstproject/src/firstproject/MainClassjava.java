2da954b5206be8404af3d61d5b04312ca247910d
sist-117 <sist-117@SIST117>
2024-04-11 12:21:02 +0900

    1234

--- firstproject/src/firstproject/MainClassjava.java
+++ firstproject/src/firstproject/MainClassjava.java
@@ -3,6 +3,6 @@ package firstproject;
 public class MainClassjava {
     public static void main(String[] args) {
 		for(int i = 1 ; i <= 10 ; i++)
-			System.out.println("Hello java!");
+			System.out.println("Hello!");
 	}
 }   
